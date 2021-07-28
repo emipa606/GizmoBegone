9b6242041f1d62cdb07a2bce7a4d99cd869ea275
emipa606 <emil.offermo@litet.net>
2021-07-28 19:56:13 +0200

    First publish

---
+++ Source/ThingBegone/Core/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using ThingBegone.Settings;
+using Verse;
+
+namespace ThingBegone.Core
+{
+    // Token: 0x02000007 RID: 7
+    [StaticConstructorOnStartup]
+    public class Startup
+    {
+        // Token: 0x06000011 RID: 17 RVA: 0x00002D80 File Offset: 0x00000F80
+        static Startup()
+        {
+            var harmony = new Harmony("com.Rimworld.Mod.ThingBegone!");
+            harmony.PatchAll();
+            IEnumerable<string> thingSaveData = LoadedModManager.GetMod<Settings.ThingBegone>()
+                .GetSettings<ThingBegoneSettings>().thingSaveData;
+            IEnumerable<string> terrainSaveData = LoadedModManager.GetMod<Settings.ThingBegone>()
+                .GetSettings<ThingBegoneSettings>().terrainSaveData;
+            foreach (var defName in thingSaveData)
+            {
+                try
+                {
+                    ThingDef.Named(defName).designationCategory = new DesignationCategoryDef
+                    {
+                        defName = "ID1924"
+                    };
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+
+            foreach (var defName2 in terrainSaveData)
+            {
+                try
+                {
+                    TerrainDef.Named(defName2).designationCategory = new DesignationCategoryDef
+                    {
+                        defName = "ID1924"
+                    };
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+    }
+}
---
+++ Source/ThingBegone/Settings/Tool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThingBegone.Settings
+{
+    // Token: 0x02000003 RID: 3
+    public static class Tool
+    {
+        // Token: 0x06000004 RID: 4 RVA: 0x000022F0 File Offset: 0x000004F0
+        public static IList<Vector2> SetVectorCount(int maxNum)
+        {
+            IList<Vector2> list = new List<Vector2>();
+            var x = 0f;
+            var y = 0f;
+            while (list.Count <= maxNum - 1)
+            {
+                list.Add(new Vector2(x, y));
+                x += 214.75f;
+                if (!(x > 644.25f))
+                {
+                    continue;
+                }
+
+                x = 0f;
+                y += 29f;
+            }
+
+            return list;
+        }
+
+        // Token: 0x06000005 RID: 5 RVA: 0x00002364 File Offset: 0x00000564
+        public static IList<Vector2> SetVectorCount(int maxNum, int secondMaxNum)
+        {
+            var list = new List<Vector2>();
+            var x = 0f;
+            var y = 0f;
+            while (list.Count <= maxNum + secondMaxNum - 1)
+            {
+                list.Add(new Vector2(x, y));
+                x += 214.75f;
+                if (!(x > 644.25f))
+                {
+                    continue;
+                }
+
+                x = 0f;
+                y += 29f;
+            }
+
+            list.RemoveRange(0, maxNum);
+            return list;
+        }
+    }
+}
